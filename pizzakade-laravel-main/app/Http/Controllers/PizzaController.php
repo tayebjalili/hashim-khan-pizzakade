b769de88d955c9d8c26aa3c60315e490f318040e
tayeb jalili <113000816+tayebjalili@users.noreply.github.com>
2024-07-29 09:49:42 +0430

    Add files via upload

---
+++ pizzakade-laravel-main/app/Http/Controllers/PizzaController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pizza;
+class PizzaController extends Controller
+{
+    public function index(){
+        $pizzas= Pizza::all();
+        return view('pizzas.index',[
+            'pizzas' => $pizzas
+        ]);
+    }
+    public function show($id){
+        $pizza = Pizza::findOrFail($id);
+        return view('pizzas.show',[
+            'pizza' => $pizza
+        ]);
+    }
+    public function create(){
+
+        return view('pizzas.create');
+    }
+    public function store(){
+
+        $pizza = new Pizza;
+
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
+
+        $pizza->save();
+
+        return redirect('/')->with('msg','Thanks For Your Order');
+    }
+    public function destroy($id){
+        $pizza = Pizza::findOrFail($id);
+        $pizza -> delete();
+
+        return redirect('/pizzas');
+    }
+}
